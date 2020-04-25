9ee2e1aeb37251bde416e33fbd3558242d5f7d31
Luigi <luismastrangelo@gmail.com>
2020-04-25 14:37:25 +0200

    Fix padding when number is greater than 100

--- src/format.ts
+++ src/format.ts
@@ -1,10 +1,9 @@
-
-///
+/// 
 export function pad0(num: number): string {
-  return ("0" + num).slice(-2)
+  return num < 100 ? '0' + num : '' + num
 }
 
-///
+/// Formats a number of seconds to '00h 00m'.
 export function hhmm(secs: number): string {
   let minutes = Math.floor(secs / 60)
   const hours = Math.floor(minutes / 60)
