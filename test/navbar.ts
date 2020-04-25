088edb43ea1d20d2ac977e320faea16acb946fc0
Luigi <luismastrangelo@gmail.com>
2020-04-25 19:44:33 +0200

    Add tests for custom elements

--- test/navbar.ts
+++ test/navbar.ts
@@ -3,3 +3,30 @@ import { colorSchemeSelect } from '../src/components'
 colorSchemeSelect('color-scheme-picker').whenChange(select => {
   console.info("Color scheme selected:", select.value)
 })
+
+
+customElements.define('chart-color-picker', class extends HTMLElement {
+  constructor() {
+    super()
+
+    const color = this.getAttribute('color')
+    // const shadowDOM = this.attachShadow({ mode: 'open' })
+
+    const select = document.createElement('select')
+    select.id = 'hola'
+    select.className = 'selectpicker'
+    // text.innerHTML = this.innerHTML
+    // this.innerHTML = ''
+
+    // const style = document.createElement('style')
+    // style.innerHTML = `p { color: ${color} }`
+
+    // shadowDOM.appendChild(style)
+    // shadowDOM.appendChild(select)
+    this.appendChild(select)
+
+
+    colorSchemeSelect('hola')
+  }
+
+})
