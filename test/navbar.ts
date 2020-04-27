76eb8322cc4f9b8d061d9d35893e90fe33862ab5
Luigi <luismastrangelo@gmail.com>
2020-04-27 09:46:45 +0200

    Remove unused component

--- test/navbar.ts
+++ test/navbar.ts
@@ -1,4 +1,3 @@
-import { colorSchemeSelect } from '../src/components2'
 
 import ChartPalettePicker from '../src/components/ChartPalettePicker.svelte'
 new ChartPalettePicker({
@@ -7,7 +6,3 @@ new ChartPalettePicker({
     // options: [{value:'asdf',data:{asdf:123}},{value:'asdasdff'}],
   },
 });
-
-colorSchemeSelect('color-scheme-picker').whenChange(select => {
-  console.info("Color scheme selected:", select.value)
-})
