0c0332ebec5c7f4dcb3325e2ac8f411728fe90ef
Luigi <luismastrangelo@gmail.com>
2020-04-29 15:11:13 +0200

    Add config for remote (local) host

--- src/index.ts
+++ src/index.ts
@@ -6,41 +6,6 @@ import commits from './data-commits.json'
 import projects from './data-projects.json'
 import workdir from './data-workdir.json'
 
-new App({
-  target: document.body,
-  props: {
-    fetchCommits: async (): Promise<typeof commits> => {
-      return commits
-    },
-    fetchProjectList: async (): Promise<string[]> => {
-      return projects.map(p => p.substring(p.lastIndexOf("/") + 1))
-    },
-    fetchWorkdirStatus: async (): Promise<typeof workdir> => {
-      return workdir
-    },
-  }
-});
-
-// export async function fetchCommits2(range): any {
-//   const commitsDataUrl = "/data/commits";
-//   const url = `${commitsDataUrl}?all&from=${range.start}&to=${range.end}`;
-//   const json = await fetch(url).then(r => r.json());
-//   return json;
-// }
-
-// export async function fetchCommits(range): any {
-//   // const url = `${commitsDataUrl}?all&from=${range.start}&to=${range.end}`;
-//   return commits
-// }
-
-// async function fetchProjectList(): Promise<string[]> {
-//   return ['proj1', 'proj2', 'proj3']
-//   // const url = "/data/projects";
-//   // const response = await fetch(url);
-//   // const json = await response.json();
-//   // return json.map(p => p.substring(p.lastIndexOf("/") + 1));
-// }
-
 // let commitsDataUrl: string
 // if (true || process.env.NODE_ENV === 'development') {
 //   commitsDataUrl = '/data/commits'
@@ -48,3 +13,35 @@ new App({
 // } else {
 //   commitsDataUrl = '/gtm-web/data-commits.json'
 // }
+
+new App({
+  target: document.body,
+  props: false ?
+    {
+      fetchCommits: async (): Promise<typeof commits> => {
+        return commits
+      },
+      fetchProjectList: async (): Promise<string[]> => {
+        return projects.map(p => p.substring(p.lastIndexOf("/") + 1))
+      },
+      fetchWorkdirStatus: async (): Promise<typeof workdir> => {
+        return workdir
+      }
+    } : {
+      fetchCommits: async (range: { start: string; end: string }): Promise<typeof commits> => {
+        const commitsDataUrl = "/data/commits"
+        const url = `${commitsDataUrl}?all&from=${range.start}&to=${range.end}`
+        const json = await fetch(url).then(r => r.json())
+        return json
+      },
+      fetchProjectList: async (): Promise<string[]> => {
+        const url = "/data/projects";
+        const response = await fetch(url);
+        const json: string[] = await response.json();
+        return json.map(p => p.substring(p.lastIndexOf("/") + 1));
+      },
+      fetchWorkdirStatus: async (): Promise<typeof workdir> => {
+        return workdir
+      },
+    }
+});
