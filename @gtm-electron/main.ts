337be310beb02f5fdd11d28d94639c610647b1ad
Luigi <luismastrangelo@gmail.com>
2020-04-30 10:24:27 +0200

    Load electron package local html

--- @gtm-electron/main.ts
+++ @gtm-electron/main.ts
@@ -14,8 +14,8 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  // mainWindow.loadFile(path.join(__dirname, "./../index.html"));
-  mainWindow.loadFile(path.join(__dirname, "../../@gtm-app/dist/index.html"));
+  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  // mainWindow.loadFile(path.join(__dirname, "../../@gtm-app/dist/index.html"));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
