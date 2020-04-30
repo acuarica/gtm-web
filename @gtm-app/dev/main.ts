7aff96f8d78a70247f6939a801831b3f89e9f139
Luigi <luismastrangelo@gmail.com>
2020-04-30 13:16:33 +0200

    Build assets into dist/assets

--- @gtm-app/dev/main.ts
+++ @gtm-app/dev/main.ts
@@ -1,6 +1,6 @@
 import '@fortawesome/fontawesome-free/css/all.css'
 
-import App from '../app/App.svelte'
+import App from '../src/App.svelte'
 
 import commits from '../data-commits.json'
 import projects from '../data-projects.json'
