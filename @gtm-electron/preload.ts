d03318548c717fc47bec7e027130e0835912a4d1
Luigi <luismastrangelo@gmail.com>
2020-04-30 08:07:27 +0200

    Use app api with mock data

--- @gtm-electron/preload.ts
+++ @gtm-electron/preload.ts
@@ -1,6 +1,8 @@
 
 import * as g from '@gtm/git'
 
+import { App, commits, projects, workdir } from '@gtm/app'
+
 window.addEventListener("DOMContentLoaded", async () => {
   const replaceText = (selector: string, text: string) => {
     const element = document.getElementById(selector);
@@ -8,6 +10,23 @@ window.addEventListener("DOMContentLoaded", async () => {
       element.innerText = text;
     }
   };
+
+
+  console.log("@preload", document.body)
+  new App({
+    target: document.body,
+    props: {
+      fetchCommits: async (): Promise<typeof commits> => {
+        return commits
+      },
+      fetchProjectList: async (): Promise<string[]> => {
+        return projects.map(p => p.substring(p.lastIndexOf("/") + 1))
+      },
+      fetchWorkdirStatus: async (): Promise<typeof workdir> => {
+        return workdir
+      }
+    }
+  })
 
   const s = g.fetchCommits()
   console.log(s, '@preload')
