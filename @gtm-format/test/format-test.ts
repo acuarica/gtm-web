d3ee2fac5bc1631bd279725a076f80263041c39d
Luigi <luismastrangelo@gmail.com>
2020-04-30 17:42:18 +0200

    Improve tests for format utilities

--- @gtm-format/test/format-test.ts
+++ @gtm-format/test/format-test.ts
@@ -1,41 +1,53 @@
-import { hhmm, pad0 } from '../src/gtm'
+import { hhmm, pad0 } from '@gtm/format'
 import assert from 'assert';
 
-describe('format', function () {
+describe('format', () => {
 
-  describe('#pad0()', function () {
+  describe('pad0', () => {
 
-    it('asdf', () => {
+    it('pads 0', () => {
       assert.equal(pad0(0), '00')
     })
 
-    it('two digits', () => {
+    it('pads one digit', () => {
       assert.equal(pad0(1), '01')
     })
 
-    it('two digits', () => {
-      assert.equal(pad0(19), '19')
+    it('pads two digits', () => {
+      assert.equal(pad0(23), '23')
     })
 
-    it('should return -1 when the value is not present', function () {
-      assert.equal(pad0(123), '123')
+    it('pads three digits', () => {
+      assert.equal(pad0(456), '456')
+    })
+
+    it('pads four digits', () => {
+      assert.equal(pad0(7890), '7890')
     })
 
   })
 
-  describe('#hhmm()', function () {
+  describe('hhmm', () => {
 
-    it('hhmm', () => {
+    it('formats less than a minute to 0', () => {
+      assert.equal(hhmm(0), '00h 00m')
+      assert.equal(hhmm(59), '00h 00m')
+    })
+
+    it('formats down to the hour', () => {
+      assert.equal(hhmm(5 * 60), '00h 05m')
+    })
+
+    it('formats down to the hour', () => {
+      assert.equal(hhmm(4 * 60 * 60 + 5 * 60), '04h 05m')
+    })
+
+    it('formats two digits in hour', () => {
       assert.equal(hhmm(12 * 60 * 60 + 5 * 60), '12h 05m')
     })
 
-    it('fetch', () => {
-      assert.equal(hhmm(12 * 60 * 60 + 5 * 60), '12h 05m')
-
-      const commitsDataUrl = "/data/projects";
-      fetch(commitsDataUrl).then(r => r.json()).then(ps => {
-        console.log(ps)
-      });
+    it('formats more than two digits in the hour', () => {
+      assert.equal(hhmm(456 * 60 * 60 + 5 * 60), '456h 05m')
     })
   })
 
