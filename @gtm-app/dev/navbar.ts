ca0c29cb0359ee3c46320a005b2d92f4a647a780
Luigi <luismastrangelo@gmail.com>
2020-05-01 17:05:53 +0200

    Remove old dev tests

--- @gtm-app/dev/navbar.ts
+++
@@ -1,8 +0,0 @@
-
-import ChartPalettePicker from '../src/components/ChartPalettePicker.svelte'
-new ChartPalettePicker({
-  target: document.getElementById('svelte-placeholder2'),
-  props: {
-    // options: [{value:'asdf',data:{asdf:123}},{value:'asdasdff'}],
-  },
-});
