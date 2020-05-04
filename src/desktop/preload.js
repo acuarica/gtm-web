0f36c88b3bad190fb458f8a0a6f8bd77dbc352c3
Luigi <luismastrangelo@gmail.com>
2020-05-04 19:09:14 +0200

    Clean up electron preload

--- src/desktop/preload.js
+++ src/desktop/preload.js
@@ -7,12 +7,6 @@ import App from '../app/App.svelte'
 import Settings from './Settings.svelte'
 
 window.addEventListener("DOMContentLoaded", async () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) {
-      element.innerText = text;
-    }
-  };
 
   console.info("Creating app with gtm/git service")
   console.log("@preload", document.body)
@@ -32,12 +26,4 @@ window.addEventListener("DOMContentLoaded", async () => {
     }
   })
 
-  // const s = g.fetchCommits()
-  // console.log(s, '@preload')
-  // const m = await s
-  // console.log(m, '@preload')
-
-  for (const type of ["chrome", "node", "electron"]) {
-    replaceText(`${type}-version`, (process.versions)[type]);
-  }
 });
