7203bf81f3db24ed11d72f3abab9389451461dab
Luigi <luismastrangelo@gmail.com>
2020-05-06 07:18:26 +0200

    Add sort project files by time

--- src/app/FileNotes.svelte
+++ src/app/FileNotes.svelte
@@ -1,14 +1,32 @@
 <script>
+  import Icon from "./Icon.svelte";
+  import { faClock } from "@fortawesome/free-solid-svg-icons/faClock";
   import { hhmm } from "@gtm/format";
 
   export let files;
+
+  let values;
+
+  function lt(left, right) {
+    return left.TimeSpent <= right.TimeSpent ? 1 : -1;
+  }
+
+  $: values = Object.values(files).sort(lt);
+
+  let asc = true;
 </script>
 
+<button
+  on:click={() => (asc = !asc)}
+  type="button"
+  class="block text-gray-400 hover:text-white focus:text-white
+  focus:outline-none" />
+Button
 <ul>
-  {#each files as file}
+  {#each values as file}
     <li class="small">
       {file.SourceFile} &nbsp;
-      <i class="fas fa-clock" />
+      <Icon class="mb-1 h-4" icon={faClock} />
       {hhmm(file.TimeSpent)}
     </li>
   {/each}
