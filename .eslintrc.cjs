da0acbdcad84f8485619d71d18b13c84761181da
Luigi <luismastrangelo@gmail.com>
2020-05-06 06:28:22 +0200

    Import rules for JS & TS to coexists

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,39 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  env: {
-    "es6": true
-  },
+  root: true,
   extends: [
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended'
   ],
-  rules: {
-  }
+  "overrides": [
+    {
+      "files": ["**/*.ts"],
+      "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+      },
+      "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended"
+      ],
+      "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+      },
+      "parser": "@typescript-eslint/parser",
+      "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module",
+        "project": "./tsconfig.json"
+      },
+      "plugins": ["@typescript-eslint"],
+      "rules": {
+        "indent": ["error", 2, { "SwitchCase": 1 }],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "single"],
+        "comma-dangle": ["error", "always-multiline"],
+        "@typescript-eslint/no-explicit-any": 0
+      }
+    }
+  ]
 };
