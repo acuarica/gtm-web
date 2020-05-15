018688747298014dc580450c7270ea7a5bc801a0
Luigi <luismastrangelo@gmail.com>
2020-05-15 19:13:57 +0200

    Add commands to get status and projects

--- gtm/src/main.rs
+++ gtm/src/main.rs
@@ -8,27 +8,39 @@ use gtm::fetch_projects;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
-struct Args {
-    command: String,
+/// The gtm Dashboard services
+///
+/// Returns gtm time data for the specified services.
+enum GtmCommand {
+    Commits {
+        #[structopt(long)]
+        from_date: String,
+        #[structopt(long)]
+        to_date: String,
+    },
+    Projects,
+    Status,
 }
 
 fn main() -> Result<(), git2::Error> {
-    let args = Args::from_args();
+    let command = GtmCommand::from_args();
 
-    match args.command.as_str() {
-        "commits" => {
+    match command {
+        GtmCommand::Commits { from_date, to_date } => {
             let repo = Repository::open("/Users/luigi/work/home").unwrap();
             let notes = gtm::get_notes(&repo).unwrap();
             let json = serde_json::to_string(&notes).unwrap();
             println!("{}", json);
         }
-        "projects" => {
+        GtmCommand::Projects => {
             let projects = fetch_projects();
             let json = serde_json::to_string(&projects).unwrap();
             println!("{}", json);
         }
-        _ => {
-            eprintln!("No option given");
+        GtmCommand::Status => {
+            // let projects = fetch_projects();
+            // let json = serde_json::to_string(&projects).unwrap();
+            println!("{}", "{}");
         }
     };
 
