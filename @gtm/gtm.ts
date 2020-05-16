4f0ea834430d82e396f1eabb88d7d4d98f2251b9
Luigi <luismastrangelo@gmail.com>
2020-05-16 05:25:19 +0200

    Remove/clean parsing notes from node
    Implemented native.

--- @gtm/gtm.ts
+++ @gtm/gtm.ts
@@ -1,5 +1,5 @@
 import { ChildProcessWithoutNullStreams } from 'child_process'
-import { Commit, WorkdirStatusList, GtmService, CommitsFilter, GtmErr, FileNote } from '@gtm/notes'
+import { Commit, WorkdirStatusList, GtmService, CommitsFilter, GtmErr } from '@gtm/notes'
 import { parseDate } from '@gtm/notes'
 
 export type GtmSpawn = (args: string[]) => ChildProcessWithoutNullStreams
@@ -65,57 +65,4 @@ export class GitService implements GtmService {
     return runGtm(this.gtm, args)
   }
 
-}
-
-export function parseFileEntry(entry: string): FileNote | null {
-  const parts = entry.split(',')
-  if (parts.length < 3) return null
-  const [sourceFile, totalTimeSpent] = parts[0].split(':')
-  if (!sourceFile) return null
-  if (!Number(totalTimeSpent)) return null
-  const status = parts[parts.length - 1]
-  if (!['m', 'r', 'd'].includes(status)) return null
-
-  const fileNote = new FileNote(
-    sourceFile,
-    Number(totalTimeSpent),
-    {},
-    status
-  )
-  for (let i = 1; i < parts.length - 1; i++) {
-    const [timeStamp, timeSpent] = parts[i].split(':')
-    if (!Number(timeSpent)) return null
-    fileNote.Timeline[timeStamp] = Number(timeSpent)
-  }
-  return fileNote
-}
-
-export function gethistory(): Promise<unknown> {
-  return Promise.resolve([])
-  // eslint-disable-next-line no-async-promise-executor
-  // return new Promise(async (resolve: any) => {
-  //   const repo = await nodegit.Repository.open('/Users/luigi/work/#archive/vscode-lgtm-ql')
-  //   const firstCommitOnMaster = await repo.getMasterCommit()
-  //   const history = firstCommitOnMaster.history();
-  //   history.on('commit', async function (commit) {
-  //     console.log('commit ' + commit.sha());
-  //     console.log('Author:', commit.author().name() +
-  //       ' <' + commit.author().email() + '>');
-  //     console.log('Date:', commit.date());
-  //     console.log('\n    ' + commit.message());
-
-  //     try {
-  //       const note = await nodegit.Note.read(repo, 'refs/notes/gtm-data', commit.id())
-  //       console.log(note.message())
-  //     } catch (e) {
-  //       // return Promise.reject()
-
-  //     }
-  //   });
-  //   history.on('end', () => {
-  //     resolve()
-  //   })
-
-  //   history.start();
-  // })
 }
