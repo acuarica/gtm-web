b662b045d2b0785ce613268fbf81c8cbc5472ba5
Luigi <luismastrangelo@gmail.com>
2020-05-20 18:45:13 +0200

    Fix update rollup dependencies for windows

--- rollup.config.js
+++ rollup.config.js
@@ -68,7 +68,7 @@ export const configs = {
       copy({
         targets: [
           { src: 'src/desktop/index.html', dest: 'dist/gtm-dash' },
-          { src: `gtmserv/target/${production ? 'release' : 'debug'}/gtmserv`, dest: 'dist/gtm-dash' },
+          { src: `gtmserv/target/${production ? 'release' : 'debug'}/gtmserv*`, dest: 'dist/gtm-dash' },
         ],
       }),
     ],
