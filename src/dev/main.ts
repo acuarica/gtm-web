7a899c5495b67673297521bf781b4fd11da00b4c
Luigi <luismastrangelo@gmail.com>
2020-05-26 20:30:03 +0200

    Fix main using webservice

--- src/dev/main.ts
+++ src/dev/main.ts
@@ -1,25 +1,16 @@
 import App from '../app/App.svelte'
-import { Commit } from '@gtm/notes';
+import { Commit, CommitsFilter, WorkdirStatusList } from '@gtm/notes';
+import { WebService } from '../app/web';
 
 console.debug('Creating main app with web service')
 
-const fetchurl = async <T>(url: string): Promise<T> => await fetch('http://localhost:8000'+url).then(r => r.json())
+const service = new WebService();
 
 new App({
   target: document.body,
   props: {
-    fetchCommits: async (range: { start: string; end: string }): Promise<Commit[]> => {
-      const commitsDataUrl = '/data/commits'
-      const url = `${commitsDataUrl}?all&from=${range.start}&to=${range.end}`
-      return fetchurl(url)
-    },
-    fetchProjectList: async (): Promise<string[]> => {
-      const url = '/data/projects';
-      return (await fetchurl<string[]>(url)).map((p: string) => p.substring(p.lastIndexOf('/') + 1));
-    },
-    fetchWorkdirStatus: async (): Promise<{ [p: string]: Commit }> => {
-      const url = '/data/status';
-      return await fetchurl(url)
-    },
+    fetchCommits: (filter: CommitsFilter): Promise<Commit[]> => service.fetchCommits(filter),
+    fetchProjectList: (): Promise<string[]> => service.fetchProjectList(),
+    fetchWorkdirStatus: (): Promise<WorkdirStatusList> => service.fetchWorkdirStatus(),
   },
 });
