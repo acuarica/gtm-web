8e68e10487695eb54e45fc658a58599dc96a3f8a
Luigi <luismastrangelo@gmail.com>
2020-05-30 02:31:49 +0200

    Add placeholder binary gtmserve

---
+++ gtmserv/src/bin/gtmserve.rs
@@ -0,0 +1,23 @@
+
+use std::{convert::Infallible, net::SocketAddr};
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+
+async fn handle(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello, World!".into()))
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(handle))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
